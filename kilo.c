34836aca8782ae4c45497121e585f0a67e0ec69e
kor5n <jegor.kotov@gmail.com>
2025-09-26 13:50:07 +0200

    added proper 'enter' sequence with splitting rows and adding new ones. Time to use this one for developing the rest of the project

--- kilo.c
+++ kilo.c
@@ -236,11 +236,13 @@ void editorUpdateRow(erow *row)
     row->rsize = idx;
 }
 
-void editorAppendRow(char *s, size_t len)
+void editorInsertRow(int at, char *s, size_t len)
 {
-    E.row = realloc(E.row, sizeof(erow) * (E.numrows +1));
+    if (at < 0 || at > E.numrows)return;
 
-    int at = E.numrows;
+    E.row = realloc(E.row, sizeof(erow) * (E.numrows +1));
+    memcpy(&E.row[at + 1], &E.row[at], sizeof(erow) * (E.numrows - at));
+
     E.row[at].size = len;
     E.row[at].chars = malloc(len + 1);
     memcpy(E.row[at].chars, s, len);
@@ -307,10 +309,28 @@ void editorInsertChar(int c)
 {
     if (E.cy == E.numrows)
     {
-        editorAppendRow(" ", 0);
+        editorInsertRow(E.numrows," ", 0);
     }
     editorRowInsertChar(&E.row[E.cy], E.cx, c);
     E.cx++;
+}
+
+void editorInsertNewline()
+{
+    if (E.cx == 0)
+    {
+        editorInsertRow(E.cy, "", 0);
+    }else
+    {
+        erow *row = &E.row[E.cy];
+        editorInsertRow(E.cy+1, &row->chars[E.cx], row->size - E.cx);
+        row = &E.row[E.cy];
+        row->size = E.cx;
+        row->chars[row->size] = '\0';
+        editorUpdateRow(row);
+    }
+    E.cy++;
+    E.cx = 0;
 }
 
 void editorDelChar()
@@ -371,7 +391,7 @@ void editorOpen(char *filename)
         {
             linelen --;
         }
-        editorAppendRow(line, linelen);
+        editorInsertRow(E.numrows,line, linelen);
     }
     free(line);
     fclose(fp);
@@ -615,7 +635,7 @@ void editorProcessKeypress()
     switch (c)
     {
         case '\r':
-            //TODO
+            editorInsertNewline();
             break;
 
         case CTRL_KEY('q'):
